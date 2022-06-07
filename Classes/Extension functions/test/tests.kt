35ae01d28d6163ef76196c655c3e5936ba4b2490
Sofia Kolchanova <sofiia.kolchanova@gmail.com>
2022-06-07 20:57:19 +0200

    fix(Tests in Classes - Extension Functions):
    added test cases to prevent tests from passing when student uses
    particular numbers in the solution instead of this, first, second
    
    Closes https://plugins.jetbrains.com/plugin/16628-kotlin-koans/reviews#review=68601

--- Classes/Extension functions/test/tests.kt
+++ Classes/Extension functions/test/tests.kt
@@ -5,10 +5,12 @@ class TestExtensionFunctions() {
     @Test(timeout = 1000)
     fun testIntExtension() {
         Assert.assertEquals("Rational number creation error: ", RationalNumber(4, 1), 4.r())
+        Assert.assertEquals("Rational number creation error: ", RationalNumber(10, 1), 10.r())
     }
 
     @Test(timeout = 1000)
     fun testPairExtension() {
         Assert.assertEquals("Rational number creation error: ", RationalNumber(2, 3), Pair(2, 3).r())
+        Assert.assertEquals("Rational number creation error: ", RationalNumber(7, 9), Pair(7, 9).r())
     }
 }
